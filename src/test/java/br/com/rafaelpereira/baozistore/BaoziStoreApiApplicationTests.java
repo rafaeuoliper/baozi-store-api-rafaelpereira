214e43e7c26e743f6cde37af50dd61e8f36363b8
rafaeuoliper <74427252+rafaeuoliper@users.noreply.github.com>
2026-03-23 23:41:42 -0300

    Add files via upload

---
+++ src/test/java/br/com/rafaelpereira/baozistore/BaoziStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelpereira.baozistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaoziStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
