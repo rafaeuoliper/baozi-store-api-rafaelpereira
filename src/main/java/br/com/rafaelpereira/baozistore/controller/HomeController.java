5204041b06e3fd32d0468b619a2258ae157f5507
rafaeuoliper <74427252+rafaeuoliper@users.noreply.github.com>
2026-03-26 02:22:05 +0000

    Projeto API Completo

---
+++ src/main/java/br/com/rafaelpereira/baozistore/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.rafaelpereira.baozistore.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API esta funcionando";
+    }
+}
